8a7907cd2af7ce25eb52d55d891807868625f7a2
David Ponarovsky <davidponar@cs.huji.ac.il>
2019-11-26 22:47:13 +0200

    before merge

--- ex2/test-ex2.sh
+++ ex2/test-ex2.sh
@@ -2,3 +2,9 @@ gcc -Wextra -Wall -Wvla -lm ./TreeAnalyzer.c -o TreeAnalyzer
 cp ./TreeAnalyzer ./ex2_test/TEST/TreeAnalyzer
 
 valgrind --leak-check=full --show-leak-kinds=all ./TreeAnalyzer ex2_test/TEST/tests/in/test_static/long_one_sided/long_one_sided.txt 0 7
+
+echo "-------"
+echo "\n"
+
+valgrind --leak-check=full --show-leak-kinds=all ./TreeAnalyzer ex2_test/TEST/tests/in/test_static/same_node_route/same_node_route.txt 0 7
+same_node_route.txt 0 0
