db565ece3bce6966cdc0593717a11b393a3794fc
David Ponarovsky <davidponar@cs.huji.ac.il>
2019-11-27 17:56:19 +0200

    before merge

--- ex2/test-ex2-re.sh
+++ ex2/test-ex2-re.sh
@@ -1,4 +1,7 @@
 gcc -Wextra -Wall -Wvla -lm ./TreeAnalyzer.c -o TreeAnalyzer
-mv ./TreeAnalyzer ./ex2_test/TEST/TreeAnalyzer
-cd ./ex2_test/TEST
-python3 ./c_ex2_tester.py 
+cp ./TreeAnalyzer ./ex2_test/TEST/TreeAnalyzer
+#cd ./ex2_test/TEST
+#python3 ./c_ex2_tester.py > log 
+cd ./tests 
+python3 -m pytest ./tester.py
+cd ../
