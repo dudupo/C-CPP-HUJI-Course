58f2e0d596b67aa84330bf5d484b882ed0c0dce5
david ponarovsky <dudupo51@gmail.com>
2019-12-27 08:51:26 +0200

    auto push

---
+++ ex4/amit_test.sh
@@ -0,0 +1,7 @@
+mv ./Activation.* ./cpp_ex1_check/in/Activation.*
+mv ./Dense.* ./cpp_ex1_check/in/Dense.*
+mv ./Matrix.* ./cpp_ex1_check/in/Matrix.*
+mv ./MlpNetwork.* ./cpp_ex1_check/in/cpp_ex1_check.*
+git add --all
+git commit -m "auto push"
+git push
